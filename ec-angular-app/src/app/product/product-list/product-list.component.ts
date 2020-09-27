654c77577f4117ce1f66de11c0afa940f3eccc0c
kazuki-h <kazuki-h@weseek.co.jp>
2020-09-27 17:26:13 +0900

    サーバーサイドとフロントエンドを接続

--- ec-angular-app/src/app/product/product-list/product-list.component.ts
+++ ec-angular-app/src/app/product/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from '../products';
+import { ProductService } from '../shared/product.service';
+
+
 
 @Component({
   selector: 'app-product-list',
@@ -7,13 +9,18 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-
   products:any
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.products = products;
+    const productObservable = this.productService.getProducts();
+    productObservable.subscribe(
+      (data)=>{ 
+        this.products = data;
+      },
+      (err)=>{ console.error(' 次のエラーが発生しました' + err); }
+    );
   }
 
 }
