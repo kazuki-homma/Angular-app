9f40468dc6f29b0f026c4b0cbe409ae3ba0dbf76
kazuki-h <kazuki-h@weseek.co.jp>
2021-04-17 10:35:57 +0900

    end point

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const config = require('./config');
 const FakeDb = require('./fake-db');
 
@@ -21,6 +22,7 @@ mongoose.connect(config.DB_URI, {
 )
 
 const app = express();
+app.use(bodyParser.json())
 
 app.use('/api/v1/products', productRoutes);
 app.use('/api/v1/users', userRoutes);
