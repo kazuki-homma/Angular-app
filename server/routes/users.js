20f3a6465f9d076ff1960afdd125c9dfa8c542f5
kazuki-h <kazuki-h@weseek.co.jp>
2021-04-18 19:30:25 +0900

    backend user create

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../model/user');
+const User = require('../model/user');
 
 router.post('/login', function(req,res){
     // Product.find({}, function(err, foundProducts){
@@ -12,13 +12,37 @@ router.post('/register', function(req,res){
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
 
-    // Product.findById(productId, function(err, foundProduct){
-    //   if(err){
-    //       return res.status(422).send({errors:[{title: 'Product Error', detail: 'Product not found'}]});
-    //   }
-     return res.json({username, email, password});
-    // })
+    if (!username) {
+        // Invalid error
+    }
+
+    if (!email) {
+        // Invalid error
+    }
+
+    if (!password) {
+        // Invalid error
+    }
+
+    if (password !== confirmPassword) {
+        // Invalid error
+    }
+
+    User.findOne({email}, function(err, foundUser){
+      if (foundUser) {
+        // Invalid error
+      }
+
+      const user = new User({username, email, password})
+      user.save(function(err) {
+          if(err) {
+              // Error message
+          }
+          return res.json({"registerd": true})
+      })
+    })
 });
 
 module.exports = router;
